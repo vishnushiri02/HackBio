7a41840368da83252cf26dc759b02c0b8f359e0a
Vishnushiri Shyamsaisundar <vishnushiri@Vishs-MacBook.fritz.box>
2025-02-04 10:31:42 +0100

    Script with info of Siri

---
+++ scripts_stage0/task_stage_0.py
@@ -0,0 +1,24 @@
+# Stage 0 task
+"""
+Use any data structure of your choice to organize the following information
+Your names
+Your slack username
+Your email
+Your hobby
+Your countries
+Your discipline
+Your preferred programming language
+Don’t use functions, loops, conditionals or any complex concepts
+Your code should include a final print statement that prints the organized 
+output in a logical and understandable way
+
+"""
+# Storing information in the datastructure
+team_lysine={"Vishnushiri":{"slack_name":"Siri","E-Mail":"vishnushiri94@gmail.com","Hobbies":"Crocheting","Country":"India","Discipline":"Bioinformatics","Programming_language":"Python"}}
+
+# Printing using f string
+
+print(f"Hi, I am Vishnushiri. \n My slack name is {team_lysine["Vishnushiri"]["slack_name"]}. \n I come from {team_lysine["Vishnushiri"]["Country"]}. \n My field of study is {team_lysine["Vishnushiri"]["Discipline"]}. \n My preferred language to code is {team_lysine["Vishnushiri"]["Programming_language"]}. \n I love doing {team_lysine["Vishnushiri"]["Hobbies"]} during my free time. \n ")
+print("***********************************************\n")
+
+# Add your print statement
