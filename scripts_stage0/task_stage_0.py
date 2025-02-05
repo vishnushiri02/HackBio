07b85495272027e27009726f1edb2a9584eec361
Johnson Nweke <36528744+Johnson3116@users.noreply.github.com>
2025-02-05 09:54:18 +0300

    Update task_stage_0.py

--- scripts_stage0/task_stage_0.py
+++ scripts_stage0/task_stage_0.py
@@ -17,7 +17,7 @@ output in a logical and understandable way
 team_lysine = {"Vishnushiri": {"slack_name": "Siri", "E-Mail": "vishnushiri94@gmail.com", "Hobbies": "Crocheting", "Country": "India", "Discipline": "Bioinformatics", "Programming_language": "Python"},
                "Oluwatobiloba": {"slack_name": "Tobijayyy", "E-Mail": "tobijohnson01@gmail.com", "Hobbies": "Reading Manhwas", "Country": "Nigeria", "Discipline": "Cell Biology and Genetics", "Programming_language": "Python"},
                "Olayemi": {"slack_name": "bakky", "E-Mail": "bakareolayemistephen@gmail.com", "Hobbies": "Reading and Playing Video Games", "Country": "Nigeria", "Discipline": "Bioinformatics", "Programming_language": "Python"}
-               }
+               }, "Johnson": {"slack_name": "Johnson", "E-mail": "nwekejj@gmail.com", "Hobbies": "Playing soccer", "Country": "Nigeria", "Discipline": "Biomedical Science/Epidemiology", "Programming_language": "Python"}
 
 # Printing using formatted string
 
@@ -33,4 +33,6 @@ print(f"Hello, I am Olayemi. \n My slack name is {team_lysine["Olayemi"]["slack_
       team_lysine["Olayemi"]["Discipline"]}. \n My preferred language to code is {team_lysine["Olayemi"]["Programming_language"]}. \n I love doing {team_lysine["Olayemi"]["Hobbies"]} during my free time. \n ")
 print("***********************************************\n")
 
-# Add your print statement
+print(f"Hello, I am Johnson. \n My slack name is {team_lysine["Johnson"]["slack_name"]}. \n I am from {team_lysine["Johnson"]["Country"]}. \n My current field of study is {
+      team_lysine["Johnson"]["Discipline"]}. \n My preferred language to code is {team_lysine["Johnson"]["Programming_language"]}. \n I love doing {team_lysine["Johnson"]["Hobbies"]} during my free time. \n ")
+print("***********************************************\n")
